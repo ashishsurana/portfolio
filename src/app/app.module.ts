5d30dc9c023e991121c45b0f20faf03164038779
Ashish Surana <ashishsurana12345@gmail.com>
2019-07-21 23:16:13 +0530

    chore(bundle): reduces bundle size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { SkillComponent } from './skills/skill.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectComponent } from './projects/project.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
 
-export const ROUTES: Routes = [
+export const ROUTES: any = [
     { path : '', component : AboutComponent },
     { path : 'about', component: AboutComponent },
-    { path : 'skills', component: SkillComponent },
     { path : 'projects', component: ProjectComponent },
     { path : 'resume', component: ResumeComponent },
     { path : 'contact', component: ContactComponent },
@@ -23,13 +19,11 @@ export const ROUTES: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-     AboutComponent, SkillComponent, ProjectComponent, ResumeComponent, ContactComponent
+     AboutComponent, ProjectComponent, ResumeComponent, ContactComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule,
-    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
